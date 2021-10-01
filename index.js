8b75fad60ad2f94275a597d4554a7168aa94c5e1
Marco <marco.silva631@outlook.com>
2021-10-01 10:39:50 -0300

    corrigindo erros na estrutura

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from "express";
-import usersRoutes from "./routes/users.js";
-import cors from "cors";
-import mongoose from "mongoose";
-import router from "./routes/users.js";
-require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
 
-app.use("/users", usersRoutes);
-app.use(express.json({ extended: true, limit: "30mb" }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(express.json());
 
+app.get('/products', (req, res)=>{
+    console.log("Achieved the root directory!");
+    res.send("Welcome to te root directory");
+})
+
+app.listen(PORT,()=>console.log(`Server running on port: http://localhost:${PORT}`));

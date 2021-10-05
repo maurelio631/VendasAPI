783f7e27d6e8b6682b3368145073db606b1c8928
Marco <marco.silva631@outlook.com>
2021-10-04 23:03:51 -0300

    Ajustando função duplicada no aquivo api

--- Views/controlevendas/src/API/products.api.js
+++ Views/controlevendas/src/API/products.api.js
@@ -5,6 +5,5 @@ const url = "http://localhost:5000/products";
 
 //Funções que vão fazer a comunicação entre a api e as views com os verbos get, post, patch e delete
 export const requestProducts = () => axios.get(url);
-export const requestProducts = () => axios.get(url);
 
 
